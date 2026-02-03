6a4b10a70e65be3918274e676656e00532f77582
Alessandro-Ugonotti <>
2026-02-03 17:36:45 +0000

    Lambda functions

--- function_as_an_argument_in_python/lambda_functions/main.py
+++ function_as_an_argument_in_python/lambda_functions/main.py
@@ -6,9 +6,9 @@ def apply_to_list(numbers, func):
 numbers = [1, 2, 3, 4, 5]
 
 # Using a lambda function to add 10 to each number
-result_add = map(apply_to_list, lambda x: x + 10)
+result_add = apply_to_list(numbers, lambda x: x + 10)
 print("Adding 10:", result_add)
 
 # Using a lambda function to multiply each number by 2
-result_multiply = map(apply_to_list, lambda x: x * 2)
+result_multiply = apply_to_list(numbers, lambda x: x * 2)
 print("Multiplying by 2:", result_multiply)
